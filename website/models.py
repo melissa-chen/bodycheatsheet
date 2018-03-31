89f9d7018fbf562347d45751bb1a58b159833123
Howard Huang <howardhuang96@gmail.com>
2018-03-31 03:38:11 -0700

    add all of django setup environment

---
+++ website/models.py
@@ -0,0 +1,5 @@
+from django.db import models
+
+class Measurement(models.Model):
+    height = models.DecimalField(max_digits=5, decimal_places=2)
+    weight = models.DecimalField(max_digits=5, decimal_places=2)
